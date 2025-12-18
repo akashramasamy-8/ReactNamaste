fd6b713013d76fe527211be2e569c92474ff9225
akashramasamy-8 <akashramasamyk@gmail.com>
2025-12-18 11:56:28 +0530

    parcel

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const heading=React.createElement("h2",{id:"heading"},"Hi guys");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading=React.createElement("h2",{},"Hi guys");
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+//root.render(heading);
 
 
-const val=React.createElement("h1",{id:"heading"},
-    [React.createElement("h2",{},"H2 tag"),
-        React.createElement("h3",{},"H3 tag")
+const val=React.createElement("div",{id:"container"},
+    [React.createElement("h2",{id:"h1tage",key:"h1"},"H2 tag"),
+        React.createElement("h3",{id:"h2tag",key:"h2"},"  H3 tag")
     ]
-)
+);
 root.render(val);
